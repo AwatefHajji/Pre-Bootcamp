10b40c87ac710087759e62b79f1313dd9b4c3b91
Awatef Hajji <awatefhajji5@gmail.com>
2023-01-28 18:39:36 +0100

    pre_BootcampAssig

---
+++ Awatef_Hajji_JS_Tinkering.js
@@ -0,0 +1,7 @@
+var childHeight = 120;
+function displayIfChildIsAbleToRideTheRollerCoaster(Height){
+    if(Height>52){console.log('Get on that ride, kiddo!')}
+    else {console.log('Sorry kiddo. Maybe next year')}
+
+}
+displayIfChildIsAbleToRideTheRollerCoaster(childHeight);
---
+++ Hajji_Awatef_JSPredict.js
@@ -0,0 +1,17 @@
+function myBirthYearFunc(){
+        console.log("I was born in " + 1980);
+    //I was born in 1980
+    }
+    function myBirthYearFunc(BirthYearInput){
+            console.log("I was born in " + BirthYearInput);
+        }
+        myBirthYearFunc(1990);//I was born in 1990
+        function add(num1, num2){
+                console.log("Summing Numbers!");
+                console.log("num1 is: " + num1);
+                console.log("num2 is: " + num2);
+                var sum = num1 + num2;
+                console.log(sum);// add tow numbers
+            }
+            
+            
